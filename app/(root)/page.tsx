e52d0a18f755fbbe822a82bc1a6b06d6020d59fe
devMorshed <ac3m0r@gmail.com>
2023-08-29 15:17:02 +0600

    Clerk Authentication Implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,14 @@
+import { UserButton, UserProfile } from "@clerk/nextjs";
+
 const Home = () => {
-  return <div className="p-4">Admin App Page</div>;
+  return (
+    <div className="p-4">
+      <h1 className="text-center font-bold text-4xl">Admin Dashboard</h1>
+      <div className="flex justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
