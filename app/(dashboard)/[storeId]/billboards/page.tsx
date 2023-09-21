8ef060c7e3d86d3b87d1a0c01782824d26dd2e65
devMorshed <ac3m0r@gmail.com>
2023-09-21 23:30:46 +0600

    sillbill

--- app/(dashboard)/[storeId]/billboards/page.tsx
+++ app/(dashboard)/[storeId]/billboards/page.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import BillboardClient from "./component/billboard-client";
-import prismadb from "@/lib/prismadb";
-import Image from "next/image";
 
 const BillboardsPage = async () => {
-  const Billb = await prismadb.billboard.findMany();
 
-  console.log(Billb);
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
